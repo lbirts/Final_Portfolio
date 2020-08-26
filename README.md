3a5ae8942c0cd38f53a48d86fe73a0096d035a41
lbirts <birtslauren@gmail.com>
2020-08-26 10:09:48 -0500

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Lauren Birts
-
-This repo holds my **Dev Landing Page**, a one stop shop to everything I do online. 🎉
-
-`master` branch has the source code.
---
+++ precache-manifest.66a3f81b9c43941fcb14756b6a56c584.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2540bff7d8890715496e4e6975535e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0738b8c1540bbf281c8",
+    "url": "/static/css/main.125dc039.chunk.css"
+  },
+  {
+    "revision": "670f87c90731e63d0818",
+    "url": "/static/js/2.b133c9f9.chunk.js"
+  },
+  {
+    "revision": "204feea506877565a2e261751145a023",
+    "url": "/static/js/2.b133c9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0738b8c1540bbf281c8",
+    "url": "/static/js/main.8e0cc323.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "d1cf3a3f302ee858e485747fea2095bb",
+    "url": "/static/media/HeadShot.d1cf3a3f.jpg"
+  },
+  {
+    "revision": "df2215ab8c9a26721be341d4bbb199c4",
+    "url": "/static/media/css.df2215ab.png"
+  },
+  {
+    "revision": "a58728919c10c74e25a9b656c478f730",
+    "url": "/static/media/jquery.a5872891.png"
+  },
+  {
+    "revision": "a935cbd91d3a2822aec34f67e00bedca",
+    "url": "/static/media/js.a935cbd9.png"
+  },
+  {
+    "revision": "fbef321e03c16bcae42560f8507757d6",
+    "url": "/static/media/python.fbef321e.svg"
+  },
+  {
+    "revision": "09043beaf51d81e2fe3b5c8f969970c5",
+    "url": "/static/media/rails.09043bea.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/static/media/redux.8251fe76.png"
+  },
+  {
+    "revision": "03af0f588a12b32e0e7e263f082c9792",
+    "url": "/static/media/ruby.03af0f58.svg"
+  },
+  {
+    "revision": "e0f651ac7aecf9187f66e5bd5f181c10",
+    "url": "/static/media/sass.e0f651ac.svg"
+  },
+  {
+    "revision": "1a350423b8ca9640930797207d6d457a",
+    "url": "/static/media/sql.1a350423.png"
+  },
+  {
+    "revision": "ddcffe46c53bccc2a127a71ce16e12e4",
+    "url": "/static/media/wordpress.ddcffe46.svg"
+  }
+]);
